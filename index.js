f834ff9b988f21d52b5f4d00bb8a6a75e7341d4c
Hamza Asif <hamzaasif0987@gmail.com>
2019-11-25 13:11:05 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onload=init();
+function init()
+{
+  var tur = new turtle();
+  tur.forward(3*50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(3*50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(3*50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(3*50);
+  tur.turnTo(90);
+  tur.forward(50);
+  tur.turnTo(90);
+  tur.forward(50);
+}
---
+++ turtle.js
@@ -0,0 +1,32 @@
+ class turtle
+{
+  constructor()
+  {
+    this.positionX=250;
+    this.positionY=300;
+    this.angle=0;
+  }
+
+  forward(length)
+  {
+    var Radperdegree = 0.017453343;
+    ctx.beginPath();
+    ctx.moveTo(this.positionX , this.positionY);
+    this.positionX = this.positionX + length*Math.cos(Radperdegree*this.angle);
+    this.positionY = this.positionY + length*Math.sin(Radperdegree*this.angle);
+    ctx.lineTo(this.positionX , this.positionY);
+    ctx.closePath();
+    ctx.stroke();
+    console.log("X: ",this.positionX ,"Y: ", this.positionY);
+  }
+  
+  turnTo(angle)
+  {
+    this.angle +=angle;
+  }
+
+   turn(angle)
+   {
+     this.angle = angle ;
+   }
+}
